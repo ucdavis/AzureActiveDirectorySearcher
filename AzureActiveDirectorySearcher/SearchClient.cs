ac6a1501e1a22021c1346267efbc383aabf22b5e
Scott Kirkland <srkirkland@gmail.com>
2016-05-18 16:44:31 -0700

    flushed out the rest

--- AzureActiveDirectorySearcher/SearchClient.cs
+++ AzureActiveDirectorySearcher/SearchClient.cs
@@ -17,10 +17,67 @@ namespace AzureActiveDirectorySearcher
             ActiveDirectoryClient = AuthenticationHelper.GetActiveDirectoryClientAsApplication(config);
         }
 
-        public async Task<IPagedCollection<IUser>> GetBySurname(string surname)
+        public async Task<IPagedCollection<IUser>> FindBySurname(string surname)
         {
-            return await ActiveDirectoryClient.Users.Where(x => x.Surname.Equals("Kirkland"))
-                .ExecuteAsync();
+            return
+                await
+                    ActiveDirectoryClient.Users.Where(x => x.Surname.Equals(surname, StringComparison.OrdinalIgnoreCase))
+                        .ExecuteAsync();
+        }
+
+        public async Task<IPagedCollection<IUser>> FindByEmail(string email)
+        {
+            return
+                await
+                    ActiveDirectoryClient.Users.Where(x => x.Mail.Equals(email, StringComparison.OrdinalIgnoreCase))
+                        .ExecuteAsync();
+        }
+
+        public async Task<IPagedCollection<IUser>> FindByKerberos(string kerb)
+        {
+            return
+                await
+                    ActiveDirectoryClient.Users.Where(
+                        x => x.ProxyAddresses.Any(a => a.Equals(GetKerberosStringForActiveDirectory(kerb))))
+                        .ExecuteAsync();
+        }
+        
+        public async Task<IPagedCollection<IUser>> FindByKerberosOrEmail(string kerb, string email)
+        {
+            return
+                await
+                    ActiveDirectoryClient.Users.Where(
+                        x => x.Mail.Equals(email, StringComparison.OrdinalIgnoreCase) ||
+                             x.ProxyAddresses.Any(
+                                 a =>
+                                     a.Equals(GetKerberosStringForActiveDirectory(kerb),
+                                         StringComparison.OrdinalIgnoreCase)))
+                        .ExecuteAsync();
+        }
+        
+        public async Task<IUser> GetSingleByKerberosOrEmail(string kerb, string email = "")
+        {
+            var result =
+                await
+                    ActiveDirectoryClient.Users.Where(
+                        x => x.Mail.Equals(email, StringComparison.OrdinalIgnoreCase) ||
+                             x.ProxyAddresses.Any(
+                                 a =>
+                                     a.Equals(GetKerberosStringForActiveDirectory(kerb),
+                                         StringComparison.OrdinalIgnoreCase)))
+                        .ExecuteAsync();
+
+            return result.CurrentPage.SingleOrDefault();
+        }
+
+        /// <summary>
+        /// Active directory doesn't have the kerb directly but we can turn it into a proxy address and search there
+        /// </summary>
+        /// <param name="kerb">kerberos ID</param>
+        /// <returns></returns>
+        private string GetKerberosStringForActiveDirectory(string kerb)
+        {
+            return string.Format("smtp:{0}@ad3.ucdavis.edu", kerb);
         }
     }
 }
---
+++ AzureActiveDirectorySearcherTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AzureActiveDirectorySearcher;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AzureActiveDirectorySearcherTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var searcher = new SearchClient(new ActiveDirectoryConfigurationValues(TestValues.TenantName, TestValues.TenantId, TestValues.ClientId, TestValues.ClientSecret));
+            
+            Assert.IsNotNull(searcher.GetSingleByKerberosOrEmail("postit", "jpknoll@ucdavis.edu").Result);
+        }
+    }
+}
