bbcd60e50ac47a4494b2da6292d64436a31fc984
Scott Kirkland <srkirkland@gmail.com>
2017-04-18 14:40:06 -0700

    remove old search project

--- AzureActiveDirectorySearch/ActiveDirectoryDefaults.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AzureActiveDirectorySearcher
-{
-    internal class ActiveDirectoryDefaults
-    {
-        public const string AzureLoginUrl = "https://login.microsoftonline.com/";
-        public const string AzureGraphResourceUrl = "https://graph.windows.net/";
-        public const string ApiVersion = "1.5";
-    }
-}
